9bdc94c0f50e45a9ff3bf85457ea63d8907d5f65
”taro <groveleffect@gmail.com>
2019-07-17 13:32:47 +0900

    パスワードのハッシュ化

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,8 @@ class User < ApplicationRecord
   validates :email, presence: true, length: {maximum: 255},
                               format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
 
+  has_secure_password
+  validates :password, presence: true, length: {minimum: 6}
+
   before_validation {email.downcase!}
 end
