ac023558647581ef0588510b6b90ac786539a913
”taro <groveleffect@gmail.com>
2019-07-17 10:23:02 +0900

    rails generateコマンドの設定変更

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,11 @@ module ImagesShere
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
 
+    config.generators do |g|
+      g.assets false
+      g.helper false
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
