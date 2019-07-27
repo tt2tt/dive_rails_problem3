da9f2bd22627f09a2010f4eb6b13c2a3ea684ccb
”taro <groveleffect@gmail.com>
2019-07-27 14:17:28 +0900

    デザインの調整

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -8,7 +8,9 @@ class PicturesController < ApplicationController
   end
 
   def show
-    @favorite = current_user.favorites.find_by(picture_id: @picture.id)
+    if logged_in?
+      @favorite = current_user.favorites.find_by(picture_id: @picture.id)
+    end
   end
 
   def new
@@ -36,9 +38,42 @@ class PicturesController < ApplicationController
   def confirm
     @picture = current_user.pictures.build(picture_params)
     @sentence = current_user.sentences.build(sentence_params)
+
+    # render :new if @sentence.invalid? || @picture.invalid?
+
+    # "渡すフォーマットの設定"
+    # respond_to do |format|
+      # "@sentenceまたは@pictureにバリテーションが発生した場合"
+    #   if @sentence.invalid? || @picture.invalid?
+    #     # "render :newさせる"
+    #     format.html { render :new }
+    #     # "渡すフォーマットにjson（配列）を指定"
+    #     # "jsonにエラーを格納"
+    #     # "ステータスコードにエラー422を指定"
+    #     # format.json { render json: (@picture.errors), status: :unprocessable_entity }
+    #     format.json { render json: @sentence.errors, status: :unprocessable_entity}
+    #   else
+    #
+    #   end
+    # end
     if @sentence.invalid? || @picture.invalid?
-      render :new
+      if @sentence.invalid?
+       flash[:errors] = @sentence.errors.full_messages
+      end
+
+      if @picture.invalid?
+        flash[:errors] = @picture.errors.full_messages
+      end
+        render :new
+      else
     end
+    # render :new if @sentence.invalid? || @picture.invalid?
+    # render :new if @picture.invalid?
+    # respond_to do |format|
+
+
+
+
     # if @picture.invalid?
     #   @sentece.invalid?
     #   render :new
@@ -65,6 +100,7 @@ class PicturesController < ApplicationController
       else
         format.html { render :new }
         format.json { render json: @picture.errors, status: :unprocessable_entity }
+        # format.json { render json: @sentence.errors, status: :unprocessable_entity}
       end
     end
   end
@@ -77,6 +113,7 @@ class PicturesController < ApplicationController
       else
         format.html { render :edit }
         format.json { render json: @picture.errors, status: :unprocessable_entity }
+        # format.json { render json: @sentence.errors, status: :unprocessable_entity}
       end
     end
   end
