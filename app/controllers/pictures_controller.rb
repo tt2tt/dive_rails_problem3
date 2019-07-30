f92bc63fb1d72e1b0bb5701738b6626e8ee85073
”taro <groveleffect@gmail.com>
2019-07-30 15:30:01 +0900

    日本語化

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -41,7 +41,7 @@ class PicturesController < ApplicationController
 
     respond_to do |format|
       if @picture.save
-        format.html { redirect_to @picture, notice: 'Picture was successfully created.' }
+        format.html { redirect_to @picture, notice: '投稿しました' }
         format.json { render :show, status: :created, location: @picture }
       else
         format.html { render :new }
@@ -53,7 +53,7 @@ class PicturesController < ApplicationController
   def update
     respond_to do |format|
       if @picture.update(picture_params)
-        format.html { redirect_to @picture, notice: 'Picture was successfully updated.' }
+        format.html { redirect_to @picture, notice: '更新しました' }
         format.json { render :show, status: :ok, location: @picture }
       else
         format.html { render :edit }
@@ -65,7 +65,7 @@ class PicturesController < ApplicationController
   def destroy
     @picture.destroy
     respond_to do |format|
-      format.html { redirect_to pictures_url, notice: 'Picture was successfully destroyed.' }
+      format.html { redirect_to pictures_url, notice: '削除しました' }
       format.json { head :no_content }
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190729133705) do
+ActiveRecord::Schema.define(version: 20190730053522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20190729133705) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "profile_image"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
