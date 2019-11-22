53a18fac347902add96e231717aba0425a4fce1c
”taro <groveleffect@gmail.com>
2019-11-22 21:42:22 +0900

    メール送信機能削除

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -38,11 +38,8 @@ class PicturesController < ApplicationController
 
   def create
     @picture = current_user.pictures.build(picture_params)
-    logger.debug @picture.errors.inspect
-
     respond_to do |format|
       if @picture.save
-        PictureMailer.contribution_mail(@picture).deliver
         format.html { redirect_to @picture, notice: '投稿しました' }
         format.json { render :show, status: :created, location: @picture }
       else
