063e3c10002f66b97264ed50f150a383323dd886
”taro <groveleffect@gmail.com>
2020-02-02 16:24:35 +0900

    再調整

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,7 +89,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'shrouded-sands-22493.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'polar-retreat-64399.herokuapp.com' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
